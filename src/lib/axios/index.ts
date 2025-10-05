55428b3c0c79854719c74757e0d39a62a22af9c8
ngtrungh1eu <hieuntse160479@fpt.edu.vn>
2025-10-05 18:02:25 +0700

    fix: :bug: minor fix

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -5,7 +5,7 @@ export const TOKEN_KEY = 'blog_app_token'
 export const REFRESH_TOKEN_KEY = 'blog_app_refresh_token'
 
 const apiInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api'
+  baseURL: process.env.NEXT_PUBLIC_API_URL
 })
 
 apiInstance.interceptors.request.use(
