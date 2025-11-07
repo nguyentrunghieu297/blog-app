874fcf557c690ad7a33dc55af0072371ff14073f
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2025-11-07 13:38:15 +0700

    fix: :bug: minor fix

--- src/utils/date-helpers.ts
+++ src/utils/date-helpers.ts
@@ -2,8 +2,8 @@ import { DateInfo } from '@/types/news'
 
 export const getTimeOfDay = (hour: number): string => {
   if (hour >= 5 && hour < 11) return 'Buổi Sáng'
-  if (hour >= 11 && hour < 14) return 'Buổi Trưa'
-  if (hour >= 14 && hour < 18) return 'Buổi Chiều'
+  if (hour >= 11 && hour < 13) return 'Buổi Trưa'
+  if (hour >= 13 && hour < 18) return 'Buổi Chiều'
   return 'Buổi Tối'
 }
 
