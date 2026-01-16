3ae21e59208004626a93abe4d006add52ae3e02a
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2026-01-16 22:36:00 +0700

    fix:

--- src/service/oil.ts
+++ src/service/oil.ts
@@ -1,11 +1,11 @@
 import apiInstance from '@/lib/axios'
-import { FuelPriceResponse } from '@/types/oil'
+import { OilPricesResponse } from '@/types/oil'
 
 const getOilPrice = async () => {
   try {
     const url = `${process.env.NEXT_PUBLIC_VIEW_OIL_API}`
 
-    const { data } = await apiInstance.get<FuelPriceResponse>(url)
+    const { data } = await apiInstance.get<OilPricesResponse>(url)
     return data?.data
   } catch (error) {
     console.error('Error fetching oil prices:', error)
