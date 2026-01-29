1cf6f319fb4dfa05f6eba3f3c6a93fe641eebb48
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2026-01-29 19:44:55 +0700

    fix: minor fix

--- src/app/news/components/news-article-skeleton.tsx
+++ src/app/news/components/news-article-skeleton.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function NewsArticleSkeleton() {
   return (
     <div className='space-y-4 py-6 border-b border-gray-200'>
-      <div className='flex gap-6'>
+      <div className='flex flex-col sm:flex-row gap-4 sm:gap-6'>
         {/* Image skeleton */}
-        <Skeleton className='w-48 h-32 rounded-lg flex-shrink-0' />
+        <Skeleton className='w-full sm:w-48 h-48 sm:h-32 rounded-lg sm:flex-shrink-0' />
+
         <div className='flex-1 space-y-3'>
           {/* Title skeleton */}
-          <Skeleton className='h-7 w-full' />
-          <Skeleton className='h-7 w-4/5' />
+          <Skeleton className='h-6 sm:h-7 w-full' />
+          <Skeleton className='h-6 sm:h-7 w-4/5' />
 
           {/* Description skeleton */}
           <div className='space-y-2 pt-2'>
@@ -20,7 +20,7 @@ export default function NewsArticleSkeleton() {
           </div>
 
           {/* Meta info skeleton */}
-          <div className='flex justify-between gap-4 pt-2'>
+          <div className='flex flex-col sm:flex-row sm:justify-between gap-2 sm:gap-4 pt-2'>
             <Skeleton className='h-3 w-24' />
             <Skeleton className='h-3 w-32' />
           </div>
